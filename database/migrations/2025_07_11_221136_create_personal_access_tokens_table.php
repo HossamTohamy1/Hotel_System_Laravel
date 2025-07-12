4522cc5560fc8ed33e9bf69c9620d39a16763a4a
HossamTohamy1 <tohamytohamy32@gmail.com>
2025-07-12 16:35:21 +0300

    migration

--- database/migrations/2025_07_11_221136_create_personal_access_tokens_table.php
+++ database/migrations/2025_07_11_221136_create_personal_access_tokens_table.php
@@ -12,14 +12,14 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('personal_access_tokens', function (Blueprint $table) {
-            $table->id();
-            $table->morphs('tokenable');
-            $table->text('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
-            $table->timestamp('expires_at')->nullable();
-            $table->timestamps();
+        $table->id();
+        $table->string('name');
+        $table->string('email')->unique();
+        $table->timestamp('email_verified_at')->nullable();
+        $table->string('password');
+        $table->enum('role', ['customer', 'staff']);
+        $table->rememberToken();
+        $table->timestamps();
         });
     }
 
